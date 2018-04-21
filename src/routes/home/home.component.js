5d66a7833d23711d6117a58b8a2a1ca31f73644b
Andrzej Kublicki <pionier2000@wp.pl>
2018-04-21 08:49:33 +0200

    Init Home component

---
+++ src/routes/home/home.component.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                Home
+            </div>
+        );
+    }
+}
+
+export default Home;
+
+// https://api.github.com/users?client_id=b7c78ac85f340b7c1de5&client_secreted08881e79718c1a45985fbfd1f84428e85198b
